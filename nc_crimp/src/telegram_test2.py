9ee987426286c516a3826a648376d5f010826fb3
owenlimm <65865177+owenlimm@users.noreply.github.com>
2023-10-25 11:12:01 +0800

    updated enum.

--- nc_crimp/src/telegram_test2.py
+++ nc_crimp/src/telegram_test2.py
@@ -2,8 +2,8 @@
 
 import rospy
 # from msg_pkg.msg import AlarmMsg
-#from robot_data_display.msg import AlarmMsg
-from nc_crimp.msg import AlarmMsg
+from robot_data_display.msg import AlarmMsg
+#from nc_crimp.msg import AlarmMsg
 from telegram import Bot
 from telegram.error import TelegramError
 # from robot_data_display.msg import CounterMsg
@@ -11,10 +11,13 @@ from telegram.error import TelegramError
 import tracemalloc
 import asyncio
 from datetime import datetime, time
+from alarm_enum import Alarm
 
 # Replace 'YOUR_BOT_TOKEN' with your actual Telegram Bot API token
 TELEGRAM_BOT_TOKEN = '6427503877:AAHg5Th8BrJianple8NoqdPAZgbcSoSf1Oc'
 CHAT_ID = '-4025128663'
+
+print ("telegram bot running...")
 
 async def send_telegram_message(msg_text):
     tracemalloc.start()
@@ -33,53 +36,11 @@ def alarm_callback(msg):
 
     alarm_value = int(msg.alarm)
 
-    if alarm_value == 9000:
-        error_msg = "Part drop while picking from left jig"
-
-    elif alarm_value == 9001:
-        error_msg = "Part drop while picking from right jig"
-
-    elif alarm_value == 9002:
-        error_msg = "Part Missing from Stamping Machine"
-    
-    elif alarm_value == 9003:
-        error_msg = "Part drop after picking from Stamping Machine"
-    
-    elif alarm_value == 9004:
-        error_msg = "Part stuck when unloading to basket"
-
-    elif alarm_value == 9005:
-        error_msg = "Part load to Stamping Machine Failed"
-
-    elif alarm_value == 9006:
-        error_msg = "Not in AUTO mode"
-
-    elif alarm_value == 9007:
-        error_msg = "Stamp Trigger Error"
-
-    elif alarm_value == 9008:
-        error_msg = "Pusher Unload Failed"
-
-    elif alarm_value == 9009:
-        error_msg = "Pusher Load Failed"
-
-    elif alarm_value == 9010:
-        error_msg = "Part drop while placing into Stamping Machine"
-
-    elif alarm_value == 9011:
-        error_msg = "Part Present Error"
-
-    elif alarm_value == 9012:
-        error_msg = "Part Present Error Dangerous"
-
-    elif alarm_value == 9013:
-        error_msg = "Robot Not at Home Position"
-    
-    elif alarm_value == 7:
-        error_msg = "Emergency Stop"
-
+    if alarm_value in [e.value for e in Alarm]:
+        # Use the Alarm enum to get the error message
+        error_msg = Alarm(alarm_value).name.replace("_", " ").title()
     else:
-        error_msg = "Error not recorded, check user manual."
+        error_msg = "Error not recorded, check user manual"
 
    # timestamps = int(msg.header.stamp.nsecs + msg.header.stamp.secs)
     #datetime.fromtimestamp(timestamps).date()
@@ -87,7 +48,7 @@ def alarm_callback(msg):
     if msg.alarm != "0" and msg.alarm !="42":
         asyncio.run(send_telegram_message(msg.alarm + '\n' + error_msg))
         
-        #rospy.loginfo(msg.alarm + '\n' + error_msg)
+        rospy.loginfo(msg.alarm + '\n' + error_msg)
          
 if __name__ == '__main__':
     rospy.init_node('alarm_msg_listener')  # Initialize the ROS node
